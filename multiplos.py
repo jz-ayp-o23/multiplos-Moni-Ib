066d08be50c5712cdee814362f46265f1de2c060
Moni-ib <monica.ibarra@iteso.mx>
2023-10-13 08:14:41 -0600

    prueba 1

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,21 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Diseña un programa para que, dados dos números enteros, determine si uno es múltiplo del otro.
 """
 
 # Declaraciones
-CONSTANTE = valor
+CONSTANTE = 0
 
 # Entradas
-entrada = input()
+numero_1 = float(input("Ingrese un número: "))
+numero_2 = float(input("Ingrese otro número: "))
 
 # Proceso
-
+if numero_1 % numero_2 == CONSTANTE or numero_2 % numero_1 == CONSTANTE:
+    mayor = max(numero_1, numero_2)
+    menor = min(numero_1, numero_2)
+    multiplo = "El número {mayor} es múltiplo del {menor}"
+else:
+    multiplo = "Ninguno de los números es múltiplo del otro"
 
 # Salidas
-print(salida)
+print(multiplo)
