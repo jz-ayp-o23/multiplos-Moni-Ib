d59399513b238161f3c032f142968670a4af31f3
Moni-ib <monica.ibarra@iteso.mx>
2023-10-13 08:16:30 -0600

    ELIF

--- multiplos.py
+++ multiplos.py
@@ -10,10 +10,10 @@ numero_1 = float(input("Ingrese un número: "))
 numero_2 = float(input("Ingrese otro número: "))
 
 # Proceso
-if numero_1 % numero_2 == CONSTANTE or numero_2 % numero_1 == CONSTANTE:
-    mayor = max(numero_1, numero_2)
-    menor = min(numero_1, numero_2)
-    multiplo = "El número {mayor} es múltiplo del {menor}"
+if numero_1 % numero_2 == CONSTANTE:
+    multiplo = "El número {numero_1} es múltiplo del {numero_2}"
+elif numero_2 % numero_1 == CONSTANTE:
+    multiplo = "El número {numero_2} es múltiplo del {numero_1}"
 else:
     multiplo = "Ninguno de los números es múltiplo del otro"
 
