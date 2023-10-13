2e8ea6e16ee1bba2db2da4799af5dccf40397215
Moni-ib <monica.ibarra@iteso.mx>
2023-10-13 08:24:27 -0600

    0

--- multiplos.py
+++ multiplos.py
@@ -14,6 +14,8 @@ if numero_1 % numero_2 == CONSTANTE:
     print(f"El número {numero_1} es múltiplo del {numero_2}")
 elif numero_2 % numero_1 == CONSTANTE:
     print(f"El número {numero_2} es múltiplo del {numero_1}")
+elif numero_1 == 0 or numero_2 == 0:
+    print("Ninguno de los números es múltiplo del otro")
 else:
     print("Ninguno de los números es múltiplo del otro")
 
